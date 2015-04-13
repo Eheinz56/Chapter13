58ac963834b3a88914e9188706b786f59583b1b9
Eric Heinzman <ehu9079@ehu9079-VirtualBox.(none)>
2015-04-13 10:31:52 -0400

    trying to do problem 1. Having trouble printing

---
+++ homework1.js
@@ -0,0 +1,12 @@
+
+function myseqSearch(arr, data) {
+    for (var i = arr.length; i > 0; ++i) {
+       if (arr[i] == data) {
+        return i;
+       }
+    }
+    return false;
+}
+
+
+print(data.length);
